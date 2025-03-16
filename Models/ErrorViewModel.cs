5b762c73df8b09628efaa72707eaeb1734d6ed2a
Yalım Fırat Çar <firatcarr34@gmail.com>
2025-03-16 18:54:12 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MenuSistemi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
