26bd96d3098d6c9780349e2316bdfcd47d4199a7
Yalım Fırat Çar <firatcarr34@gmail.com>
2025-03-16 19:49:44 +0300

    Editör Sayfası açıldı, Kategori ekleme, güncelleme ve silme işlemleri eklendi.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,7 +40,50 @@ namespace MenuSistemi.Controllers
             return View(viewModel);
         }
 
-       
+        public IActionResult Editor(int? Id)
+        {
+            var categoryList = _connection.Query<Category>("SELECT * FROM Category").ToList();
+            var menuLeftJoin = _connection.Query<MenuLeftJoinCategory>("SELECT * FROM Menu LEFT JOIN Category ON Menu.CategoryId = Category.Id").ToList();
+            var viewModel = new MenuAndCategory()
+            {
+
+                Category = categoryList,
+                MenuLeftJoin = menuLeftJoin
+
+            };
+
+
+            ViewBag.Id = Id;
+            return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            var addCategory = _connection.Execute
+                (
+                  @"INSERT INTO Category
+                    (CategoryName)
+                    VALUES
+                    (@CategoryName)", category
+                );
+            TempData["AddCategory"] = " Category Ekleme Ýþlemi Baþarýyla iþlemi tamamlandý.";
+            return RedirectToAction("Editor");
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {
+            var updateCategory = _connection.Execute
+             (
+               @"UPDATE  Category
+               SET
+               CategoryName = @CategoryName
+               WHERE
+               Id=@Id", category
+               );
+            TempData["UpgradeCategory"] = " Category Güncelleme Ýþlemi Baþarýyla iþlemi tamamlandý.";
+            return RedirectToAction("Editor");
+        }
 
     }
 }
